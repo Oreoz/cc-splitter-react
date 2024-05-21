861ef87058961862740803ea1f0ac4624399f733
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2024-05-21 14:29:03 -0400

    debt: remove styled-components from app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
 import { SplitProvider } from "./components/SplitProvider";
 import { Navbar } from "./nav";
 import { Index } from "./routes/Index";
 import { Splitting } from "./routes/Splitting";
 
-const FlexWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const FillViewport = styled.div`
-  display: flex;
-  flex: 1;
-`;
-
 function App() {
   return (
-    <FlexWrapper>
+    <div>
       <Navbar />
 
-      <FillViewport>
-        <SplitProvider>
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Index} />
-              <Route path="/splitting" component={Splitting} />
-            </Switch>
-          </Router>
-        </SplitProvider>
-      </FillViewport>
-    </FlexWrapper>
+      <SplitProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Index} />
+            <Route path="/splitting" component={Splitting} />
+          </Switch>
+        </Router>
+      </SplitProvider>
+    </div>
   );
 }
 
